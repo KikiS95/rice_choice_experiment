f3cde6f39f6420f20ed908c26d50b60fb46377fb
KikiS95 <52923896+KikiS95@users.noreply.github.com>
2019-07-30 21:18:30 +0100

    mlogit() works without asc variable

--- test.R
+++ test.R
@@ -1,1 +1,14 @@
-test script
+ricedata <-read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_complete.csv")
+str(ricedata)
+ricedata[2:14]<- lapply(ricedata[5:14], as.numeric)
+ricedata$asc <-as.character(ricedata$asc)
+ricedata$alt <-as.integer(ricedata$alt)
+str(ricedata)
+
+riceml <- mlogit.data(ricedata, choice = "choice", shape = "long", alt.levels = c("alt1", "alt2", "alt3"))
+head(riceml)
+
+
+criceml <- mlogit(choice ~ frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+criceml <- mlogit(choice ~  asc +frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+
