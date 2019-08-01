7a5a01c6c4ebe27595dda7d662daa18c54104305
kota <k.mameta78@gmail.com>
2019-08-01 10:33:08 +0900

    Update test.R

--- test.R
+++ test.R
@@ -1,16 +1,22 @@
 ricedata <-read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_complete.csv")
-str(ricedata)
-ricedata[2:14]<- lapply(ricedata[5:14], as.numeric)
-ricedata$asc <-as.character(ricedata$asc)
-ricedata$alt <-as.integer(ricedata$alt)
+# ricedata <- read_csv("~/Dropbox/Shared_NIES/Shared_KiranAgrPJ/DataAnalysis/ce_complete.csv")
+# kota read file
 str(ricedata)
 
-riceml <- mlogit.data(ricedata, choice = "choice", shape = "long", alt.levels = c("alt1", "alt2", "alt3"))
-head(riceml)
+#--- Comment:: I cannot understand the code follow...
+#### what do you want to do their code ?
 
+# ricedata[2:14]<- lapply(ricedata[5:14], as.numeric)
+# ricedata$asc <-as.character(ricedata$asc)
+# ricedata$alt <-as.integer(ricedata$alt)
+# str(ricedata)
 
-criceml <- mlogit(choice ~ frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
-criceml <- mlogit(choice ~ asc + frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+rice <- data.table::data.table(ricedata)
+riceml <- mlogit::mlogit.data(rice, choice = "choice", shape = "long",alt.levels = "alt", id.var = "STR",varying=4:15)
+
+riceml
+criceml <- mlogit::mlogit(choice ~ frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data =riceml )
+criceml <- mlogit::mlogit(choice ~ as.factor(alt) + frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
 
 
 # Tried clogit instead of mlogit
