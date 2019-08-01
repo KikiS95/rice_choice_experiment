bc34b79775a8c490e8701bd1e820ba4cf9d4ef49
kota <k.mameta78@gmail.com>
2019-08-01 21:07:21 +0900

    Update test.R

--- test.R
+++ test.R
@@ -12,24 +12,49 @@ str(ricedata)
 
 rice <- data.table::data.table(ricedata)
 
+<<<<<<< HEAD
 riceml <- mlogit::mlogit.data(rice, choice = "choice", shape = "long",alt.levels = c("alt1","alt2","alt3"), id.var = "STR",varying=4:16)
 criceml <- mlogit::mlogit(choice ~ frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data =riceml )
 summary(criceml)
 
 criceml2 <- mlogit::mlogit(choice ~ as.factor(alt) + frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+=======
+criceml <- mlogit(choice ~ frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+criceml <- mlogit(choice ~ asc + frog + bird + fish + location1 + cultivation1 + price | 0 | 0, data = riceml)
+>>>>>>> master
 
 
 # Tried clogit instead of mlogit
 
 library(survival)
-ricedata <-read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_complete.csv")
+ricedata <-read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_demographic_complete.csv")
 criceml1 <- clogit(choice ~ asc + frog + bird + fish + location1 + cultivation1 + price + strata(STR), data = riceml)
 summary(criceml1)
 
-
+#replaced bird with fish
 library(survival)
 ricedata <-read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_complete.csv")
 criceml2 <- clogit(choice ~ asc + frog + fish + location1 + cultivation1 + price + strata(STR), data = riceml)
 summary(criceml2)
 
 
+## price:male
+rice_demo <- read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_demographic_complete.csv")
+criceml4 <- clogit(choice ~ asc + frog + fish + location1 + cultivation1 + price + price:male +strata(STR), data = rice_demo)
+summary(criceml4)
+
+##price:none
+rice_demo <- read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_demographic_complete.csv")
+criceml5 <- clogit(choice ~ asc + frog + fish + location1 + cultivation1 + price + price:none +strata(STR), data = rice_demo)
+summary(criceml5)
+
+##price:ricebuyer1 price:ricebuyer2 price:ricebuyer3
+rice_demo <- read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_demographic_complete.csv")
+criceml6 <- clogit(choice ~ asc + frog + fish + location1 + cultivation1 + price + price:rice_buyer1 + price:rice_buyer2 + price:rice_buyer3 +strata(STR), data = rice_demo)
+summary(criceml6)
+
+##price:rice_frequency
+rice_demo <- read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/rice_ce/ce_demographic_complete.csv")
+rice_demo$rice_frequency <-as.factor(rice_demo$rice_frequency) 
+criceml7 <- clogit(choice ~ asc + frog + fish + location1 + cultivation1 + price + rice_frequency + price:rice_frequency + strata(STR), data = rice_demo[!is.na(rice_demo$age),])
+summary(criceml7)
